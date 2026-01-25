8f84dfcd93478eae9332b888e77f92bd47803189
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2026-01-25 19:27:37 +0530

    animated hero asterik svg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,24 @@
+"use client"
 import React from "react";
 import { ContainerScroll } from "./ui/container-scroll-animation";
 import Image from "next/image";
 import heroImage from "@/public/images/hero.png";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+
 
 const Hero = () => {
+
+    useGSAP(() => {
+        const tl1 = gsap.timeline();
+        tl1.from(".hero-svg", {
+            rotate: 360,
+            repeat: -1,
+            duration: 7,
+            ease: "none"
+        });
+    })
     return (
         <section className="flex flex-col items-center justify-center px-4 text-center overflow-hidden">
             <ContainerScroll
@@ -13,7 +28,7 @@ const Hero = () => {
                         <h1 className="flex flex-wrap items-center justify-center gap-4 text-7xl md:text-8xl font-medium tracking-tighter text-secondary mb-12">
                             <span>Dev Toolkit</span>
                             <span className="text-accent flex items-center justify-center relative">
-                                <svg width="0.8em" height="0.8em" viewBox="0 0 256 256" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className="animate-spin-slow">
+                                <svg width="0.8em" height="0.8em" viewBox="0 0 256 256" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className="animate-spin-slow hero-svg">
                                     <path d="M 152 70.059 L 201.539 20.519 L 235.48 54.461 L 185.941 104 L 256 104 L 256 152 L 185.941 152 L 235.48 201.539 L 201.539 235.48 L 152 185.941 L 152 256 L 104 256 L 104 185.941 L 54.46 235.48 L 20.52 201.539 L 70.059 152 L 0 152 L 0 104 L 70.059 104 L 20.519 54.46 L 54.461 20.52 L 104 70.059 L 104 0 L 152 0 Z" fill="currentColor"></path>
                                 </svg>
                             </span>
