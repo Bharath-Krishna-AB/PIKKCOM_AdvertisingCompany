c97c88d71fb6a5b6b9182a34f34bc27f549d55d9
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2026-02-04 12:54:48 +0530

    ficed background issue

--- components/NewHero.tsx
+++ components/NewHero.tsx
@@ -94,17 +94,6 @@ const NewHero = () => {
             className="relative w-full h-screen flex flex-col items-center justify-center overflow-hidden text-secondary"
         >
 
-            {/* Background Gradient/Noise Parallax */}
-            <div
-                className="absolute inset-0 z-0 pointer-events-none"
-            >
-                <div className="absolute top-[-20%] left-[-20%] w-[80vw] h-[80vw] bg-accent/5 rounded-full blur-[100px] mix-blend-multiply filter" />
-                <div className="absolute bottom-[-20%] right-[-20%] w-[60vw] h-[60vw] bg-secondary/5 rounded-full blur-[80px] mix-blend-multiply filter" />
-                <div className="absolute inset-0 opacity-[0.04]"
-                    style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")` }}
-                />
-            </div>
-
             {/* Centered Content Group */}
             <div className="relative z-10 flex flex-col items-center justify-center w-full h-full pointer-events-none">
 
